7ccc35eb24a822dd7612f09e5295bb5325a5badd
Iiro Anttila <iiro.a.anttila@gmail.com>
2025-10-14 19:13:17 +0300

    Added basic addition and subtraction.

--- src/main/java/org/example/Variables/Num/NumHandler.java
+++ src/main/java/org/example/Variables/Num/NumHandler.java
@@ -4,9 +4,27 @@ import static org.example.InputReader.integers;
 
 public class NumHandler {
     public void handleNum(String line){
-        String[] parts = line.split("=");
-        String name = parts[0].replace("num", "").trim();
-        Integer value = Integer.parseInt(parts[1].replace("=", "").trim());
-        integers.put(name, value);
+        if (line.contains("=")){
+            String[] parts = line.split("=");
+            String name = parts[0].replace("num", "").trim();
+            Integer value = Integer.parseInt(parts[1].replace("=", "").trim());
+            integers.put(name, value);
+        } else if (line.contains("+")) {
+            String[] parts = line.split("\\+");
+            String num1 = parts[0].replace("num", "").trim();
+            String num2 = parts[1].trim();
+
+            if (integers.containsKey(num1) && integers.containsKey(num2)){
+                System.out.println(integers.get(num1) + integers.get(num2));
+            }
+        } else if (line.contains("-")) {
+            String[] parts = line.split("\\-");
+            String num1 = parts[0].replace("num", "").trim();
+            String num2 = parts[1].trim();
+
+            if (integers.containsKey(num1) && integers.containsKey(num2)){
+                System.out.println(integers.get(num1) - integers.get(num2));
+            }
+        }
     }
 }
